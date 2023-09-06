77607e16220940ce0352f7bd33410d10f099a6f3
Wala121 <144242708+Wala121@users.noreply.github.com>
2023-09-06 21:53:30 +0300

    A-Update Sorter.java

--- Sorter.java
+++ Sorter.java
@@ -1,36 +1,37 @@
 public class Sorter {
-    public static void insertionSort(int[] arr) {
+   public static void selectionSort(int[] arr) {
         int n = arr.length;
         
-        for (int i = 1; i < n; i++) {
-            int key = arr[i];
-            int j = i - 1;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
             
-            // Move elements of arr[0..i-1] that are greater than key
-            // to one position ahead of their current position
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
+            // Find the index of the minimum element in the unsorted part of the array
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
             }
             
-            arr[j + 1] = key;
+            // Swap the found minimum element with the first element in the unsorted part
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
     }
 
     public static void main(String[] args) {
-        int[] arr = {12, 11, 13, 5, 6};
+        int[] arr = {64, 25, 12, 22, 11};
 
         System.out.println("Original array:");
         for (int num : arr) {
             System.out.print(num + " ");
         }
         
-        insertionSort(arr);
+        selectionSort(arr);
 
         System.out.println("\nSorted array:");
         for (int num : arr) {
             System.out.print(num + " ");
         }
     }
-}
 }
